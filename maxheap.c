9663254a6ee0862c20099e520d6ce2d433f018ac
KosMosV2 <jesusm1948@gmail.com>
2024-08-08 16:29:23 -0400

    Added heap initialize function

--- maxheap.c
+++ maxheap.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define CAP 3
+// #define MAXSIZE 20
+
 typedef struct Heap Heap;
 
 struct Heap {
@@ -20,7 +24,18 @@ void front(Heap * heap);
 void cleanHeap(Heap * heap);
 
 Heap * createHeap(){
-    
+    //Allocate memory for heap
+    Heap * newHeap = (Heap *) malloc(sizeof(Heap));
+
+    //Initialize heap beginner values
+    newHeap->size = 0;
+    newHeap->cap = CAP;
+
+    //Initialize Heap Array
+    newHeap->arr = (int *) malloc(sizeof(int) * newHeap->cap);
+
+    return newHeap;
+
 }
 
 
